f71260cdc80b6f6b4c5784d5c7746924d5f2c52a
Jonathan van Rijn <jhonniedj@outlook.com>
2017-11-17 14:49:24 +0100

    -

---
+++ MqtPi-PC/MqtPi-PC/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace MqtPi_PC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.DNStextBox = new System.Windows.Forms.TextBox();
+            this.IPtextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 49);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(215, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 100);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(215, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(12, 151);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(215, 45);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // DNStextBox
+            // 
+            this.DNStextBox.Location = new System.Drawing.Point(48, 12);
+            this.DNStextBox.Name = "DNStextBox";
+            this.DNStextBox.Size = new System.Drawing.Size(92, 20);
+            this.DNStextBox.TabIndex = 4;
+            this.DNStextBox.Text = "micropi";
+            this.DNStextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // IPtextBox
+            // 
+            this.IPtextBox.Location = new System.Drawing.Point(172, 12);
+            this.IPtextBox.Name = "IPtextBox";
+            this.IPtextBox.Size = new System.Drawing.Size(100, 20);
+            this.IPtextBox.TabIndex = 5;
+            this.IPtextBox.Text = "x.x.x.x";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "DNS";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "IP";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MqtPi_PC.Properties.Resources._1474952477502;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 283);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(233, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(233, 111);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 20);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Tag = "";
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(233, 162);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 20);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 549);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(87, 54);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "lamp";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(47, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(105, 549);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(87, 54);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "versterker";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(47, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "on";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 639);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPtextBox);
+            this.Controls.Add(this.DNStextBox);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TextBox DNStextBox;
+        private System.Windows.Forms.TextBox IPtextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MqtPi-PC/MqtPi-PC/Form1.cs
@@ -0,0 +1,318 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqtPi_PC
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //300; 594
+            DNS_resolve();
+            Connect();
+
+            brush1 = new SolidBrush(Color.FromArgb(255,0,0,0));
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(brush1, new Rectangle());
+
+
+            Controls.Add(invisibleButton);
+            invisibleButton.Click += delegate { MessageBox.Show("Hello World"); };
+            invisibleButton.FlatStyle = FlatStyle.Flat;
+            invisibleButton.BackColor = BackColor;
+            invisibleButton.FlatAppearance.BorderColor = BackColor;
+            invisibleButton.FlatAppearance.MouseOverBackColor = BackColor;
+            invisibleButton.FlatAppearance.MouseDownBackColor = BackColor;
+            invisibleButton.Size = new Size(50, 50);
+            invisibleButton.Location = new Point(139, 227);
+
+        }
+        SolidBrush brush1;
+        Graphics formGraphics;
+        Button invisibleButton = new Button();
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+
+            //this.BackColor = c;
+            //DNStextBox.BackColor = Color.FromArgb(c.R, c.G, c.B);
+            send(c);
+
+            brush1 = new SolidBrush(c);
+            formGraphics = this.CreateGraphics();
+            textBox1.Text = trackBar1.Value.ToString();
+            textBox2.Text = trackBar2.Value.ToString();
+            textBox3.Text = trackBar3.Value.ToString();
+            invisibleButton.BackColor = c;
+            invisibleButton.FlatAppearance.BorderColor = c;
+            invisibleButton.FlatAppearance.MouseOverBackColor = c;
+            invisibleButton.FlatAppearance.MouseDownBackColor = c;
+            formGraphics.FillRectangle(brush1, new Rectangle(139, 227, 50, 50));
+            
+            Thread.Sleep(500);
+        }
+
+        Point cursor = new Point();
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            GetCursorPos(ref cursor);
+
+            var c = GetColorAt(cursor);
+
+            //this.BackColor = c;
+            //DNStextBox.BackColor = Color.FromArgb(c.R, c.G, c.B);
+            send(c);
+            update(c);
+
+            brush1 = new SolidBrush(c);
+            formGraphics = this.CreateGraphics();
+            textBox1.Text = trackBar1.Value.ToString();
+            textBox2.Text = trackBar2.Value.ToString();
+            textBox3.Text = trackBar3.Value.ToString();
+            formGraphics.FillRectangle(brush1, new Rectangle(92, 227, 50,50));
+
+        }
+
+        private void send(Color cc)
+        {
+            Publish(cc.R.ToString(), "gpio/red");
+            Publish(cc.G.ToString(), "gpio/green");
+            Publish(cc.B.ToString(), "gpio/blue");
+        }
+        private void update(Color cc)
+        {
+            trackBar1.Value = cc.R;
+            trackBar2.Value = cc.G;
+            trackBar3.Value = cc.B;
+        }
+
+        Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using (Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero))
+                {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //Console.WriteLine("Red: "+trackBar1.Value);
+            Publish(trackBar1.Value.ToString(), "gpio/red");
+            textBox1.Text = trackBar1.Value.ToString();
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            //Console.WriteLine("Green: " + trackBar2.Value);
+            Publish(trackBar2.Value.ToString(), "gpio/green");
+            textBox2.Text = trackBar2.Value.ToString();
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            //Console.WriteLine("Blue: " + trackBar3.Value);
+            Publish(trackBar3.Value.ToString(), "gpio/blue");
+            textBox3.Text = trackBar3.Value.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //DNS_resolve();
+        }
+
+        MqttClient client;
+        IPAddress ip_address;
+        bool connected = false;
+        void Connect()
+        {
+            // create client instance 
+            ip_address = IPAddress.Parse(IPtextBox.Text);//when dns resolve fails
+                                                          //DNS_resolve();
+
+            // Ping's the local machine.
+            Ping pingSender = new Ping();
+            
+            IPAddress address = ip_address;
+            PingReply reply = pingSender.Send(address,100);
+            //PingReply reply = pingSender.Send(DNSeditText.Text);
+
+            if (reply.Status == IPStatus.Success)
+            {
+                client = new MqttClient(ip_address.ToString());
+
+                string clientId = Guid.NewGuid().ToString();
+
+                client.Connect(clientId);
+                IPtextBox.BackColor=Color.DarkGreen;
+
+                connected = true;
+                Console.WriteLine("Address: {0}", reply.Address.ToString());
+                Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+                Subscribe();
+            }
+            else
+            {
+                connected = false;
+                Console.WriteLine(reply.Status);
+                IPtextBox.BackColor = Color.DarkRed;
+                MessageBox.Show("Did not connect!", "Ping!");
+            }
+        }
+
+        void DNS_resolve()
+        {
+            //Console.WriteLine("DNS SET TO:" + DNSeditText.Text);
+            IPAddress ip = ip_address;
+
+            try
+            {
+                IPHostEntry host;
+                host = Dns.GetHostEntry(DNStextBox.Text);
+                Console.WriteLine("GetHostEntry({0}) returns:", DNStextBox.Text);
+                foreach (IPAddress ipip in host.AddressList)
+                {
+                    Console.WriteLine("    {0}", ipip);
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException caught!!!");
+                Console.WriteLine("Source : " + e.Source);
+                Console.WriteLine("Message : " + e.Message);
+
+                MessageBox.Show(e.Message, "DNS Resolve");
+            }
+
+            try
+            {
+                IPHostEntry hostInfo = Dns.GetHostEntry(DNStextBox.Text);
+                IPAddress[] address = hostInfo.AddressList;
+
+                if (address.Length > 0) { ip_address = address[0]; ip = address[0]; }
+            }
+            catch { }
+
+            //if (ip == ip_address) { Console.WriteLine("IP THA SAME"); }
+            //Console.WriteLine("IP SET TO:" + ip_address);
+            //RunOnUiThread(() => {  });
+            if (ip_address != null) { IPtextBox.Text = ip_address.ToString(); }
+        }
+
+
+        void Subscribe()//string topic
+        {
+            if (connected)
+            {
+                // register to message received
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                //string clientId = Guid.NewGuid().ToString();
+                //client.Connect(clientId);
+
+                // subscribe to the topic "/home/temperature" with QoS 2
+                client.Subscribe(new string[] { "deur" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+            }
+        }
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string output = Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine(output);
+
+            if (output.Contains("tring!"))
+            {
+                MessageBox.Show("Er staat iemand bij de deur!", "Deurbel");
+            }
+            ///DNSeditText.Text = output;
+            // handle message received
+        }
+
+        void Publish(string message, string topic)
+        {
+            if (connected)
+            {
+                string strValue = Convert.ToString(message);
+
+                // publish a message on "/home/temperature" topic with QoS 2 
+                client.Publish(topic, Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            DNS_resolve();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Publish("1", "gpio/lamp");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Publish("0", "gpio/lamp");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Publish("1", "gpio/amp");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Publish("0", "gpio/amp");
+        }
+    }
+}
